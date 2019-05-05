5d21c5ed130a9f4f2a91e878afb83d59e7a11945
Zane Dennis <zaneddennis@gmail.com>
2019-05-05 18:14:35 -0400

    removed artifacts from ADS.py and improved documentation

--- ADS.py
+++ ADS.py
@@ -1,33 +1,12 @@
 import pandas as pd
 import warnings
 from sklearn.preprocessing import LabelEncoder
-#from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 warnings.filterwarnings("ignore")
 
-# takes a DataFrame
-# returns a regressor
-"""def simpleLR(data):
-    le = LabelEncoder()
-    le.fit(data.sex.drop_duplicates())
-    data.sex = le.transform(data.sex)
 
-    le.fit(data.smoker.drop_duplicates())
-    data.smoker = le.transform(data.smoker)
-
-    le.fit(data.region.drop_duplicates())
-    data.region = le.transform(data.region)
-
-    x = data.drop(["charges"], axis=1)
-    y = data.charges
-
-    #x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
-    lr = LinearRegression().fit(x, y)
-
-    return lr"""
-
-# takes a DataFrame
+# takes a DataFrame containing all of the data
 # returns the DataFrame transformed using the ADS' process
 def preprocess(data):
     le = LabelEncoder()
@@ -42,6 +21,8 @@ def preprocess(data):
 
     return data
 
+# takes a DataFrame containing the training data
+# returns a fitted sklearn LinearRegression object
 def simpleLR(data):
     x = data.drop(["charges"], axis=1)
     y = data.charges
